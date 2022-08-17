1bbbe6222a3cbcb5c4d8bedc49690e94132dc35c
Shakya2k <42931272+Shakya2k@users.noreply.github.com>
2022-08-17 13:08:36 +0530

    Time: 44 ms (22.41%), Space: 13.5 MB (43.14%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.py
+++ 804-unique-morse-code-words/804-unique-morse-code-words.py
@@ -7,13 +7,21 @@ class Solution(object):
         morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
         
         words_morse = []
+        
+        #first loop to traverse along 'words'
         for word in words:
+            
             word_morse = ""
-            for letter in word:
-                tab = ord(letter) - 97
+            
+            for letter in word: #second loop to traverse along each word in 'words'
+                
+                tab = ord(letter) - ord('a') #getting index of letter using ASCII
                 word_morse += morse[tab]
-            words_morse.append(word_morse)
-        words_morse = set(words_morse)
-        return len(words_morse)
+                
+            words_morse.append(word_morse) #storing converted morse word
+            
+        words_morse = set(words_morse) #using set to store only unique values
+        
+        return len(words_morse) #returning length of the final set to get result
         
         
