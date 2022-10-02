96a4bc7f7af77bb3432a68d124bafdc144e2eff0
Shakya2k <42931272+Shakya2k@users.noreply.github.com>
2022-10-03 00:02:52 +0530

    Time: 68 ms (20.56%), Space: 13.9 MB (46.14%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,18 +1,14 @@
-
-
-class Solution(object):
-    def numDecodings(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        dp = [0] * (len(s) + 1)
-        dp[0] = 1
-        dp[1] = 1 if s[0] != '0' else 0
-        for i in range(2, len(s) + 1):
-            if s[i - 1] != '0':
-                dp[i] += dp[i - 1]
-            if s[i - 2] != '0' and eval(s[i - 2] + s[i - 1]) < 27:
-                dp[i] += dp[i - 2]
-        return dp[len(s)]
-        
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if len(s) == 0 or s[0] == '0':
+            return 0
+        oneBack = 1
+        twoBack = 1
+        for i in range(1, len(s)):
+            cur = 0
+            if s[i] != '0':
+                cur += oneBack
+            if s[i-1] != '0' and int(s[i-1:i+1]) <= 26:
+                cur += twoBack
+            oneBack, twoBack = cur, oneBack
+        return oneBack
